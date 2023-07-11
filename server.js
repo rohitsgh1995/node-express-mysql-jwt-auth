3de6933edba2abf7640f909fdf655a65a6c0a670
Rohit Singh <rohit1995sgh@gmail.com>
2023-07-11 16:20:50 +0530

    routes

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to my world." });
 });
 
+// routes
+require("./routes/auth.routes")(app);
+require("./routes/user.routes")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
